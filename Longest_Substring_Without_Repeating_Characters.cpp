042a04bc55758214c6b7bb312b010805ed02a824
utwodownson <utwodownson@gmail.com>
2013-08-21 00:25:16 +0800

    2013/08/21  00:24:03

--- Longest_Substring_Without_Repeating_Characters.cpp
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -4,5 +4,27 @@
 
 int find_longest(char *a)
 {
-    bool visit
+    bool visit[256];
+    memset(visit, false, sizeof(visit));
+
+    int max = 0, i = 0, j = 0;
+    while (j < strlen(a)) {
+        while (j < strlen(a) && !visit[a[j]])  // forget the j < len
+            visit[a[j++]] = true;
+
+        if (max < j - i) 
+            max = j - i;
+
+        do {
+            visit[a[i]] = false;
+        } while (a[i++] != a[j]);
+    }
+    return max;
+}
+
+int main()
+{
+    char a[10010];
+    while (scanf("%s", a) != EOF) 
+        printf("%d\n", find_longest(a));
 }
